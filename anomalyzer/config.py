e3a1a583076efd8eb972910d9e76c603ba237981
emirka <emirka@gmail.com>
2019-09-13 16:52:39 -0400

    Added data investigation notebook

---
+++ anomalyzer/config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 14:15:10 2019
+
+@author: emirk
+"""
+
+#config.py 
+
+import os
+
+data_directory='C:/Users/emirk/OneDrive/Documents/GitHub/insight/data/raw'
+file_name='Tuesday-WorkingHours.pcap_ISCX.csv'
+
+data_file=os.path.join(data_directory,file_name)
